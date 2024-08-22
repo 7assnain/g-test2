93cc927fb2c52593d5ac880fcfbbb3b09107ca89
hassnain <m.7assnainprogramer@gmail.com>
2024-08-22 11:10:29 +0200

    test

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>g-test</title>
     <link rel="stylesheet" href="CSS/main.css">
+    <link rel="stylesheet" href="CSS/lang.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -17,7 +18,7 @@
             </div>
             <div class="searchbar"></div>
             <div class="headerlinks">
-            <div class="lang">lang</div>
+            <div class="langfield">lang</div>
             <div class="headershopcart"></div>
             <div class="signcontainer">
             <div class="signinheader"><a href="login.php" class="Signinlink">Sign In</a></div>
@@ -25,6 +26,7 @@
             </div>
             </div>
         </header>
+        <h2 class="product">product</h2>
     </div>
     <script src="JS/main.js"></script>
 </body>
