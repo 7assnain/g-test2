77abe8fd4531d5e36d7089469e830de2c2f4dbdd
7anan <7anan@email.com>
2024-08-27 12:01:30 +0200

    .

--- top_menu.php
+++ top_menu.php
@@ -5,7 +5,7 @@ session_start()
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Registration and login in PHP</title>
+        <title>Home Page</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
