9ccc859bbef9a4b8a9cc045d0277f8bffaeb27f9
hassnain <m.7assnainprogramer@gmail.com>
2024-08-29 17:34:24 +0200

    view

--- shopcart.php
+++ shopcart.php
@@ -1,1 +1,16 @@
 <?php include('top_menu.php')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>shop cart</title>
+</head>
+<body>
+    <header>
+    <div class="headerlogo">
+            <a href="products.php">G</a>
+        </div>
+    </header>
+</body>
+</html>
